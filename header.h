bba7ce31d110fbdfd610a0c3b16b10fd3ff24d83
Tim <timothee.mariotte@edu.ece.fr>
2023-11-12 23:24:53 +0100

    Test 2 Timothée 12/11/2023 23:24

---
+++ header.h
@@ -0,0 +1,8 @@
+//
+// Created by mario on 12/11/2023.
+//
+
+#ifndef MAIN_C_HEADER_H
+#define MAIN_C_HEADER_H
+
+#endif //MAIN_C_HEADER_H
--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    printf("Ceci est un test\n");
+    printf("Ceci est un test 2\n");
     return 0;
 }
